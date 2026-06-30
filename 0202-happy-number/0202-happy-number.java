8b93637bb6bca99cbe49a9d60b7206daaf793118
Jil Vaghasiya <jilvaghasiya14@gmail.com>
2026-06-30 05:39:54 +0530

    Time: 1 ms (78.09%), Space: 42.8 MB (11.98%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    private int ssum(int n){
+        int sum = 0;
+        while (n>0){
+            int digit = n%10;
+            sum+=digit*digit;
+            n/=10;
+        }
+        return sum;
+    }
+    public boolean isHappy(int n) {
+        HashSet<Integer> set = new HashSet<>();
+        while(n!=1 && !set.contains(n)){
+            set.add(n);
+            n = ssum(n);
+        }
+        return n ==1;
+    }
+}
